65228bda98cf786e662b2bd6738b537d9f07cf2c
Luis Salas <cslesel24@gmail.com>
2025-11-30 17:45:56 -0500

    modificacion allowed host

--- administracion/administracion/settings.py
+++ administracion/administracion/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@-lbouasfikoan^67n$v&ynuo*v75_fnr#fh8vho62is9w0^$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gestorevento-backend.onrender.com']
 
 
 # Application definition
